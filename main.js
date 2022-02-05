0e3b89905794b80174e01b66cea9a5d5058af9b3
Kirill <dremkiri@gmail.com>
2022-02-06 00:48:56 +0200

    1

---
+++ main.js
@@ -0,0 +1,92 @@
+// Синтаксис цикла While
+// переменная
+// начальное значение
+// while(условие){
+//     инструкция
+//     сччетчик
+// }
+
+// let a = 0;
+// while(a < 5){
+//     console.log(a)
+//     a++;
+// }
+
+
+// do...while
+// первоначальнгое условие (переменая)
+// do {
+//     инструкция
+//     счетчик
+// } while (условие);
+
+// let a = 0;
+// do {
+//     a++
+//     console.log(a)
+// } while (a < 0);
+
+// let a = 1
+// let sum = 0
+// let message = +prompt('Final sum')
+
+// while(a <= message){
+//  sum += a
+//  a++
+// }
+// console.log(sum)
+
+// let x = 1
+// while(x <= 10){
+//     kvadr = x ** 2;
+//     x++
+//     console.log(`num ${x}, square = ${kvadr}`)
+// }
+
+
+//Задание 1 
+// let n = +prompt("Enter n");
+// let s = 0
+// let i = 1;
+// let a = 1;
+
+// while (i <= n) {
+//     s = s + a;
+//     i++;
+//     a = a + 2
+// }
+// console.log(s)
+
+
+// Задание 2
+// let n = 1;
+// let mull = 1
+// let k = +prompt("Ведите факориал числа k!=");
+
+// while (n <= k) {
+//     mull*=n 
+//     n++;
+// }
+// console.log(mull)
+
+// Задание 3
+// let n = +prompt("Enter n");
+// let s = 0
+// let i = 1;
+// let a = 0.5;
+// while (i <= n) {
+//     s = s + a;
+//     i++;
+//     a = a / 2
+// }
+// console.log(s)
+
+
+// Задание 4
+// let n = +prompt('Enter n')
+// let a = 0
+// while(n >= 0){
+//     n = n / 10;
+//     a++
+// }
+// alert(a)
